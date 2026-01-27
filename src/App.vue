c503386aa6540e41f106256749df0fd5b6552c7b
oriolfiblasancho <oriolfibla@iesmontsia.org>
2026-01-27 18:02:44 +0100

    feat: initialize Vue 3 project with Vite and TypeScript
    - Add package.json with project metadata and dependencies
    - Create favicon.ico for the project
    - Implement main App.vue component with basic template
    - Set up main.ts to bootstrap the Vue application
    - Configure TypeScript with tsconfig.app.json and tsconfig.node.json
    - Add Vite configuration in vite.config.ts for Vue and dev tools

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>XAVIER QUE ME FUNCIONA EL FRONTEND</h1>
+  <p>
+    Vaig a provar la api a ver si retorna algo
+  </p>
+  
+</template>
+
+<style scoped></style>
